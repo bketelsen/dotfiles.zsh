43867c4dd78512d7af4c5791de87eb24bbf07bed
Brian Ketelsen <bketelsen@gmail.com>
2021-11-16 11:33:32 -0500

    fix lunar installer

--- scripts/lvim/install.sh
+++ scripts/lvim/install.sh
@@ -2,9 +2,9 @@
 
 bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
 
-mkdir -P ~/.config/lvim/
+mkdir -p ~/.config/lvim/
 ln -s "$BJK_DOTFILES"/scripts/lvim/config.lua ~/.config/lvim/config.lua
 ln -s "$BJK_DOTFILES"/scripts/lvim/ignore ~/.config/lvim/ignore
 
-mkdir -P ~/.config/lvim/plugin/
+mkdir -p ~/.config/lvim/plugin/
 ln -s "$BJK_DOTFILES"/scripts/lvim/packer_compiled.lua ~/.config/lvim/plugin/packer_compiled.lua
