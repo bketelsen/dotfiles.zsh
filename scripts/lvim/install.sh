cc5cf5c00a394af137f16b0add6c4757bfb3a569
Brian Ketelsen <bketelsen@gmail.com>
2021-11-16 11:37:06 -0500

    more fixing

--- scripts/lvim/install.sh
+++ scripts/lvim/install.sh
@@ -3,6 +3,9 @@
 bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
 
 mkdir -p ~/.config/lvim/
+rm -rf ~/.config/lvim/config.lua
+rm -rf ~/.config/lvim/ignore
+rm -rf ~/.config/lvim/plugin
 ln -s "$BJK_DOTFILES"/scripts/lvim/config.lua ~/.config/lvim/config.lua
 ln -s "$BJK_DOTFILES"/scripts/lvim/ignore ~/.config/lvim/ignore
 
